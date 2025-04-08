244fc1a84f029930bad379e2a0a93c7a940b9689
Vanessa Rådström <vanessaradstrom@n169-p53.eduroam.kth.se>
2025-04-08 13:53:03 +0200

    ändrat

--- src/main.c
+++ src/main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include <SDL.h>
-#include <SDL2/SDL.h>
+ #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 960
 
+int initilize();
+
 int main()
 {
-    
+    return initilize();
 }
 
-int initilize ()
+int initilize()
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
     {
         printf("error initializing SDL: %s\n ", SDL_GetError());
         return 1;
     }
-    SDL_Window* win = SDL_CreateWindow("HELLO",
+    SDL_Window *win = SDL_CreateWindow("HELLO",
                                        SDL_WINDOWPOS_CENTRED,
                                        SDL_WINDOW_CENTERD,
                                        WINDOW_WIDTH, WINDOW_HEIGHT, 0);
@@ -31,24 +32,23 @@ int initilize ()
     }
 
     Unit32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    SDL_Renderer *rend = SDL_CreateRenderer(win, -1, render_flags);
     if (!rend)
     {
         SDL_DestroyWindow(win);
         SDL_Quit();
         return 1;
     }
-    
-   /* SDL_Surface* surface = IMG_Load("??????");
-    if (!surface)
-    {
-        printf("eroor creating surface\n");
-        SDL_DestroyRenderer(rend);
-        SDL_DestroyWindoe(win);
-        SDL_Quit();
-        return 1;
-    }*/
-    
+
+    /* SDL_Surface* surface = IMG_Load("??????");
+     if (!surface)
+     {
+         printf("eroor creating surface\n");
+         SDL_DestroyRenderer(rend);
+         SDL_DestroyWindoe(win);
+         SDL_Quit();
+         return 1;
+     }*/
 
     SDL_Delay(5000);
     SDL_DestroyWindow(win);
