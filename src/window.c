9198d5fa3d826ab32ecc90b62d483b098f13bf1c
Vanessa Rådström <vanessaradstrom@n169-p53.eduroam.kth.se>
2025-04-08 12:59:44 +0200

    Gjort om window som vår main och börjat på vår main fil

--- src/window.c
+++ src/window.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 960
+
 int main()
+{
+    
+}
+
+int initilize ()
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
     {
@@ -18,9 +26,32 @@ int main()
     {
         printf("error creating window: %s\n", SDL_GetError());
         SDL_Quit();
-        return 1; 
+        return 1;
+    }
+
+    Unit32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    if (!rend)
+    {
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
     }
     
+   /* SDL_Surface* surface = IMG_Load("??????");
+    if (!surface)
+    {
+        printf("eroor creating surface\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindoe(win);
+        SDL_Quit();
+        return 1;
+    }*/
+    
+
+    SDL_Delay(5000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     printf("Initialization successful!\n");
     SDL_Quit();
