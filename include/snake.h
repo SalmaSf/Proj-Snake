389394088cc4290a3cdf9c131bde39deb6a3f0e8
Naher <naher.islam@hotmail.com>
2025-04-10 13:40:03 +0200

    Wrap-around, men v1

--- include/snake.h
+++ include/snake.h
@@ -6,7 +6,8 @@
 typedef struct segment Segment;
 typedef struct snake Snake;
 
-Snake *createSnake(int x, int y, SDL_Renderer *pRenderer);
+Snake *createSnake(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height);
+
 void updateSnake(Snake *pSnake);
 void drawSnake(Snake *pSnake);
 void destroySnake(Snake *pSnake);
--- src/snake.c
+++ src/snake.c
@@ -17,9 +17,11 @@ struct snake
     SDL_Texture *pTexture;
     SDL_Rect headRect;
     float speed;
+    int window_width;  // New
+    int window_height; // New
 };
 
-Snake *createSnake(int x, int y, SDL_Renderer *pRenderer)
+Snake *createSnake(int x, int y, SDL_Renderer *pRenderer, int window_width, int window_height)
 {
     Snake *pSnake = malloc(sizeof(Snake));
     pSnake->head = malloc(sizeof(Segment));
@@ -41,6 +43,9 @@ Snake *createSnake(int x, int y, SDL_Renderer *pRenderer)
     pSnake->headRect.w /= 7;
     pSnake->headRect.h /= 7;
 
+    pSnake->window_width = window_width;
+    pSnake->window_height = window_height;
+
     return pSnake;
 }
 
@@ -49,19 +54,25 @@ void updateSnake(Snake *pSnake)
     int mouseX, mouseY;
     SDL_GetMouseState(&mouseX, &mouseY);
 
+    static float lastAngle = 0.0f;
     float dx = mouseX - pSnake->head->x;
     float dy = mouseY - pSnake->head->y;
-    float angle = atan2f(dy, dx);
+
+    if (mouseX >= 0 && mouseX <= pSnake->window_width &&
+        mouseY >= 0 && mouseY <= pSnake->window_height)
+    {
+        lastAngle = atan2f(dy, dx);
+    }
 
     float distance = sqrtf(dx * dx + dy * dy);
 
-    pSnake->head->x += cosf(angle) * pSnake->speed;
-    pSnake->head->y += sinf(angle) * pSnake->speed;
+    float moveX = cosf(lastAngle) * pSnake->speed;
+    float moveY = sinf(lastAngle) * pSnake->speed;
 
     if (distance > pSnake->speed)
     {
-        float moveX = cosf(angle) * pSnake->speed;
-        float moveY = sinf(angle) * pSnake->speed;
+        float moveX = cosf(lastAngle) * pSnake->speed;
+        float moveY = sinf(lastAngle) * pSnake->speed;
 
         pSnake->head->x += moveX;
         pSnake->head->y += moveY;
@@ -71,6 +82,20 @@ void updateSnake(Snake *pSnake)
         pSnake->head->x = mouseX;
         pSnake->head->y = mouseY;
     }
+
+    pSnake->head->x += moveX;
+    pSnake->head->y += moveY;
+
+    // Wrap-around
+    if (pSnake->head->x < 0)
+        pSnake->head->x += pSnake->window_width;
+    else if (pSnake->head->x > pSnake->window_width)
+        pSnake->head->x -= pSnake->window_width;
+
+    if (pSnake->head->y < 0)
+        pSnake->head->y += pSnake->window_height;
+    else if (pSnake->head->y > pSnake->window_height)
+        pSnake->head->y -= pSnake->window_height;
 }
 
 void drawSnake(Snake *pSnake)
