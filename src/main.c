ec79640e5335696b1d7cebc1bb7ec120bb51d3a5
Naher <naher.islam@hotmail.com>
2025-04-28 20:57:49 +0200

    Klient kod

--- src/main.c
+++ src/main.c
@@ -1,12 +1,88 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_net.h>
+#include <string.h>
 #include <stdbool.h>
 #include "snake.h"
 #include "bakgrund.h"
 #include "meny.h"
 
+#define SERVER_IP "192.168.1.138"
+#define SERVER_PORT 12345
+
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 700;
+
+static UDPsocket udpSocket;
+static IPaddress serverAddr;
+static UDPpacket *packet;
+
+int initSnakeClient()
+{
+    if (SDLNet_Init() < 0)
+    {
+        SDL_Log("SDLNet_Init: %s\n", SDLNet_GetError());
+        return 0;
+    }
+
+    udpSocket = SDLNet_UDP_Open(0);
+    if (!udpSocket)
+    {
+        SDL_Log("SDLNet_UDP_Open: %s\n", SDLNet_GetError());
+        return 0;
+    }
+
+    if (SDLNet_ResolveHost(&serverAddr, SERVER_IP, SERVER_PORT) < 0)
+    {
+        SDL_Log("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+        return 0;
+    }
+
+    packet = SDLNet_AllocPacket(512);
+    if (!packet)
+    {
+        SDL_Log("SDLNet_AllocPacket: %s\n", SDLNet_GetError());
+        return 0;
+    }
+
+    packet->address.host = serverAddr.host;
+    packet->address.port = serverAddr.port;
+
+    return 1;
+}
+
+void sendSnakePosition(int x, int y)
+{
+    struct
+    {
+        int x, y;
+    } SnakeData;
+    SnakeData.x = x;
+    SnakeData.y = y;
+    memcpy(packet->data, &SnakeData, sizeof(SnakeData));
+    packet->len = sizeof(SnakeData);
+    SDLNet_UDP_Send(udpSocket, -1, packet);
+}
+
+void receiveServerUpdate()
+{
+    if (SDLNet_UDP_Recv(udpSocket, packet))
+    {
+        struct
+        {
+            int x, y;
+        } serverData;
+        memcpy(&serverData, packet->data, sizeof(serverData));
+        printf("Received from server: x=%d y=%d\n", serverData.x, serverData.y);
+    }
+}
+
+void closeSnakeClient()
+{
+    SDLNet_FreePacket(packet);
+    SDLNet_UDP_Close(udpSocket);
+    SDLNet_Quit();
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +92,17 @@ int main(int argc, char *argv[])
 
     SDL_Window *pWindow = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!initSnakeClient())
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Network Error", "Failed to initialize network connection.", pWindow);
+        SDL_DestroyRenderer(pRenderer);
+        SDL_DestroyWindow(pWindow);
+        IMG_Quit();
+        SDLNet_Quit();
+        SDL_Quit();
+        return 1;
+    }
 
     if (!visaStartMeny(pRenderer))
     {
