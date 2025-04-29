1bc8666ef4f4a49760743cad8c106b34c6cae7ec
Bushra <bushraa@ug.kth.se>
2025-04-29 15:47:48 +0200

    timern 3

--- src/main.c
+++ src/main.c
@@ -142,8 +142,8 @@ int main(int argc, char *argv[])
 
         int mouseX, mouseY;
         SDL_GetMouseState(&mouseX, &mouseY);
-        sendSnakePosition(mouseX, mouseY);
-        receiveServerUpdate();
+        //sendSnakePosition(mouseX, mouseY);
+        //receiveServerUpdate();
 
         updateSnake(pSnake);
 
