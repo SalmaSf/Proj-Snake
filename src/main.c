d510c4f4bc29468603f50249751a6463ee57b849
Alina98k <alifak626@gmail.com>
2025-05-23 15:24:51 +0200

    win/lose

--- src/main.c
+++ src/main.c
@@ -203,7 +203,8 @@ void runGame(Game *pGame)
 // Endast visa vinnarbild om spelaren vann (dvs sista som lever)
             if (result.win) {
                 int choice = showResult(pGame->pRenderer, true, result.time);
-                if (choice == 0)
+                if (choice == 0);
+            }
         }
     }
 }
